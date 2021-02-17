165b99e700e254738c88bac63363f26e18b84d76
btbrad <biantaobrad@gmail.com>
2021-02-18 00:24:07 +0800

    columnList组件完成

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,12 +1,16 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title">
-      <h5>{{ column.title }}</h5>
-      <p>{{ column.description }}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar" class="rouded-circle border border-light w-25 my-3" :alt="column.title">
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,7 +18,7 @@ import { defineComponent, PropType } from 'vue'
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
 
@@ -24,6 +28,17 @@ export default defineComponent({
     list: {
       type: Array as PropType<ColumnProps[]>,
       required: true
+    }
+  },
+  setup (props) {
+    const columnList = props.list.map(item => {
+      if (!item.avatar) {
+        item.avatar = require('@/assets/default-avatar.jpeg')
+      }
+      return item
+    })
+    return {
+      columnList
     }
   }
 })
