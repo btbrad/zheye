f1847ba4f6352006c84f7aac0be0b0ca9c709fbf
btbrad <biantaobrad@gmail.com>
2021-02-20 20:38:00 +0800

    validateInput实现v-model

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <form>
       <div class="mb-3">
         <label class="form-label">邮箱地址</label>
-        <validate-input :rules="emailRules"></validate-input>
+        <validate-input :rules="emailRules" v-model="emailValue"></validate-input>
       </div>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
@@ -68,6 +68,7 @@ export default defineComponent({
     ValidateInput
   },
   setup () {
+    const emailValue = 'bt@163.com'
     const emailRules: RuleProps = [
       { type: 'required', message: '电子邮箱地址不能为空' },
       { type: 'email', message: '请输入正确的电子邮箱格式' }
@@ -92,7 +93,8 @@ export default defineComponent({
       currentUser,
       emailRef,
       validateEmail,
-      emailRules
+      emailRules,
+      emailValue
     }
   }
 })
--- src/components/ValidateInput.vue
+++ src/components/ValidateInput.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="validate-input-container pb-3">
-    <input type="text" :class="['form-control', {'is-invalid': inputRef.error}]" v-model="inputRef.val" @blur="validateInput" />
+    <input
+      type="text"
+      :class="['form-control', {'is-invalid': inputRef.error}]"
+      :value="inputRef.val"
+      @blur="validateInput"
+      @input="updateValue"
+    />
     <span v-if="inputRef.error" class="invalid-feedback">{{ inputRef.message }}</span>
   </div>
 </template>
@@ -17,14 +23,20 @@ export type RuleProps = RuleProp[]
 export default defineComponent({
   name: 'ValidateInput',
   props: {
-    rules: Array as PropType<RuleProps>
+    rules: Array as PropType<RuleProps>,
+    modelValue: String
   },
-  setup (props) {
+  setup (props, context) {
     const inputRef = reactive({
-      val: '',
+      val: props.modelValue || '',
       error: false,
       message: ''
     })
+    const updateValue = (event: KeyboardEvent) => {
+      const targetValue = (event.target as HTMLInputElement).value
+      inputRef.val = targetValue
+      context.emit('update:modelValue', targetValue)
+    }
     const validateInput = () => {
       if (props.rules) {
         const allPassed = props.rules.every(rule => {
@@ -48,7 +60,8 @@ export default defineComponent({
     }
     return {
       inputRef,
-      validateInput
+      validateInput,
+      updateValue
     }
   }
 })
